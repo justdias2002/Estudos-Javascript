a86b407ae58f25b43b6beb72c63e2ad3de375805
João Victor Dias <jotavaresvictor@gmail.com>
2024-07-13 22:22:18 -0300

    .

--- 3_ESTRUTURAS/js/script.js
+++ 3_ESTRUTURAS/js/script.js
@@ -19,10 +19,62 @@ console.log(typeof idade);
 // let 2teste = "invalido"; 
 // let @teste = "invalido";
 
-
-
-
-
 let a = 3, b = 2, c = 10;
 console.log(a * b)
 
+const nomecompleto = "João Dias";
+
+const nomeCompleto = "João Victor";
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "olk"
+
+console.log(_teste, $teste);
+
+// prompt
+
+// const age =  prompt("Digite a sua idade:");
+
+// console.log(`Você tem ${age} anos.`)
+
+
+// // alert
+// alert("Testando")
+
+
+// math
+console.log(Math.max(5, 2, 1 , 10));
+
+console.log(Math.floor(5.14));
+
+console.log(Math.ceil(5.14));
+
+// console
+
+console.log("Teste");
+
+console.error("erro!")
+
+console.warn("aviso!"); 
+
+
+
+// if
+
+const m = 10;
+
+if(m > 5){
+
+    console.log("M maior que 5!")
+
+}
+
+const user = "joão";
+
+if(user === "joão") {
+    console.log("Bem vindo João")
+}
