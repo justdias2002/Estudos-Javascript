541ee4d56357c675724889a7b046d6a4663b3b3f
João Victor Dias <jotavaresvictor@gmail.com>
2024-07-25 14:33:33 -0300

    Update script.js

--- 4_FUNÇOES/js/script.js
+++ 4_FUNÇOES/js/script.js
@@ -173,3 +173,50 @@ function someFunction() {
 }
 
 someFunction();
+
+// masi sobre closure
+
+const multiplicationClosure = (n) => {
+  return (m) => {
+    return n * m;
+  };
+};
+
+const c1 = multiplicationClosure(5);
+const c2 = multiplicationClosure(10);
+
+console.log(c1);
+console.log(c2);
+
+console.log(c1(5));
+console.log(c2(10));
+
+// recursão
+
+const untilTen = (n, m) => {
+  if (n < 10) {
+    console.log("A função parou de executar!");
+  } else {
+    const x = n - m;
+    console.log(x);
+
+    untilTen(x, m);
+  }
+};
+
+untilTen(100, 7);
+
+
+
+function factorial(x) {
+  if(x === 0) {
+    return 1
+  } else {
+    return x * factorial(x - 1)
+  }
+}
+
+const num = 6
+const resulta = factorial(num)
+
+console.log(`O fatorial do numero ${num} é ${resulta}`)
