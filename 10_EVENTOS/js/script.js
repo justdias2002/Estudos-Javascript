f6e28389d9f075bda03427e4419798d04950c792
João Victor Dias <jotavaresvictor@gmail.com>
2024-08-25 12:40:57 -0300

    .

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -15,9 +15,35 @@ function imprimirImagem() {
 
 secondBtn.addEventListener("click", imprimirImagem);
 
-
 const thirdBtn = document.querySelector("#other-btn");
 
-thirdBtn.addEventListener("click", () =>{
-    console.log("Evento removido")
+thirdBtn.addEventListener("click", () => {
+  console.log("Evento removido");
+  secondBtn.removeEventListener("click", imprimirImagem);
+});
+
+// argumento especial chamado event
+
+const myTitle = document.querySelector("#my-title");
+
+myTitle.addEventListener("click", (event) => {
+  console.log(event);
+  console.log(event.offsetX);
+  console.log(event.pointerType);
+  console.log(event.target);
+});
+
+// propragaçao
+
+const containerBtn = document.querySelector("#btn-container");
+const btnInsindeContainer = document.querySelector("#div-btn");
+
+
+containerBtn.addEventListener("click", () => {
+  console.log("Evento 1")
+})
+
+btnInsindeContainer.addEventListener("click", (e) => {
+  e.stopPropagation()
+  console.log("Evento 2")
 })
