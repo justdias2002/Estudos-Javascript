4278ee35eef858bf78f28fbf125d9755f89da2bb
João Victor Dias <jotavaresvictor@gmail.com>
2024-09-03 23:55:06 -0300

    .

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -38,12 +38,76 @@ myTitle.addEventListener("click", (event) => {
 const containerBtn = document.querySelector("#btn-container");
 const btnInsindeContainer = document.querySelector("#div-btn");
 
-
 containerBtn.addEventListener("click", () => {
-  console.log("Evento 1")
-})
+  console.log("Evento 1");
+});
 
 btnInsindeContainer.addEventListener("click", (e) => {
-  e.stopPropagation()
-  console.log("Evento 2")
-})
+  e.stopPropagation();
+  console.log("Evento 2");
+});
+
+// removendo padrao
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  console.log("Não alterou a pagina");
+});
+
+// eventos de teclado
+document.addEventListener("keyup", (e) => {
+  console.log(`Soltou a tecla ${e.key}`);
+});
+document.addEventListener("keydown", (e) => {
+  console.log(`Apertou a tecla ${e.key}`);
+});
+
+// eventos de Mouse
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () => {
+  console.log("Pressionou o Botão");
+});
+
+mouseEvents.addEventListener("mouseup", () => {
+  console.log("Soltou o Botão");
+});
+
+mouseEvents.addEventListener("dbclick", () => {
+  console.log("Clique duplo");
+});
+
+// movimento do mouse
+
+document.addEventListener("mousemove", (e) => {
+  // console.log(`No eixo x: ${e.x}`)
+  // console.log(`No eixo y: ${e.y}`)
+});
+
+// evento de foco
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+  console.log("Em foco");
+});
+
+// debounce
+const debounce = (f, delay) => {
+  let timeout;
+
+  return (...arguments) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      f.apply(arguments);
+    }, delay);
+  };
+};
+
+window.addEventListener("mousemove", debounce(()=> {
+    console.log("Executando a cada 400ms")
+}, 400))
