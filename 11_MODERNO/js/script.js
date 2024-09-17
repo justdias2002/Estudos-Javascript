889ec8f500ce91af52f5cb37597be39de36d3e62
João Victor Dias <jotavaresvictor@gmail.com>
2024-09-16 22:53:48 -0300

    Update script.js

--- 11_MODERNO/js/script.js
+++ 11_MODERNO/js/script.js
@@ -74,3 +74,92 @@ const highNumbers = arr.filter((n) => {
   }
 });
 console.log(highNumbers);
+
+const users = [
+  { name: "João", available: true },
+  { name: "Sol", available: false },
+  { name: "Maria", available: true },
+  { name: "Felipe", available: false },
+  { name: "Matheus", available: true },
+];
+
+const availableUsers = users.filter((user) => user.available);
+
+console.log(availableUsers);
+
+// map
+
+const products = [
+  { name: "Camisa", price: 10.99, category: "Roupas" },
+  { name: "Chaleira ", price: 150, category: "Eletro" },
+  { name: "Fogão", price: 1999.99, category: "Eletro" },
+  { name: "Calça", price: 69.99, category: "Roupas" },
+];
+
+products.map((product) => {
+  if (product.category === "Roupas") {
+    product.onSale = true;
+  }
+});
+
+console.log(products);
+
+// template literals
+const userName = "João";
+const age = 21;
+console.log(`O nome do usuario é ${userName} e ele tem ${age} anos`);
+
+// destructig
+
+const fruits = ["Maçã", "Mamão", "Abacate"];
+
+const [f1, f2, f3] = fruits;
+console.log(f1, f3);
+
+const productDetails = {
+  name: "Mouse",
+  price: 39.9,
+  category: "Periferico",
+  color: "Cinza",
+};
+
+const { name: productName, price, category, color } = productDetails;
+
+console.log(
+  `O nome do produto é ${productName} e ele custa ${price} e é da cor ${color}`
+);
+
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+
+const a3 = [...a1, ...a2];
+
+console.log(a3);
+
+const a4 = [0, ...a1, 4];
+
+console.log(a4);
+
+const carName = { name: "Gol" };
+const carBrand = { brand: "VW" };
+const otherInfos = { km: 100000, price: 49000 };
+
+const car = { ...carName, ...carBrand, ...otherInfos };
+
+console.log(car);
+
+// 8 classes
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  productWithDiscount(discount) {
+    return this.price * ((100 - discount) / 100);
+  }
+}
+
+const shirt = new Product("Camisa gola V", 20);
+
+console.log(shirt.productWithDiscount(10));
